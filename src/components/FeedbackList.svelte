1e6ae10d49a2587141640e6b85f2681eee4316e2
Mayk Brito <maykbrito@gmail.com>
2022-07-23 16:39:53 -0300

    feedback stats

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,8 +1,10 @@
 <script>
 	import { Feedback } from '../store'
     import FeedbackItem from './FeedbackItem.svelte';
+    import FeedbackStats from './FeedbackStats.svelte';
 </script>
 
+<FeedbackStats />
 {#each $Feedback as fb (fb.id)} 
     <FeedbackItem item={fb} />
 {/each}
---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {Feedback} from '../store'
+  
+    $: count = $Feedback.length
+    $: average = $Feedback.reduce((a, {rating}) => a + rating, 0) / $Feedback.length
+  </script>
+  
+  <div class="feedback-stats">
+    <h4>{count} Reviews</h4>
+    <h4>Ratings Average: {average}</h4>
+  </div>
+  
+  <style>
+    .feedback-stats {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  </style>
